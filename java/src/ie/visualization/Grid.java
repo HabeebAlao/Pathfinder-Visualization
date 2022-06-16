deea11ecc4fb15fca4ec3264418ffa1bb0854e17
habeeb <habeebalao101@gmail.com>
2022-06-16 21:01:14 +0100

    file setup

---
+++ java/src/ie/visualization/Grid.java
@@ -0,0 +1,30 @@
+package ie.visualization;
+
+
+import processing.core.PApplet;
+
+public class Grid extends PApplet {
+
+    public void drawGrid() {
+
+    }
+
+    public void settings() {
+        size(1000, 1000);
+
+    }
+
+    public void keyPressed() {
+
+    }
+
+    public void setup() {
+        colorMode(RGB);
+
+    }
+
+    public void draw() {
+        background(0);
+
+    }
+}
---
+++ java/src/ie/visualization/Main.java
@@ -0,0 +1,28 @@
+/*
+Program Description: Path finder visualization
+
+Development Environment: This program was developed in Visual Studio Code.
+
+Author: Habeeb Alao
+
+Created: 11/06/2022    Modified: 16/06/2022
+*/
+
+package ie.visualization;
+
+public class Main
+{
+    public void PathVisualization()
+    {
+        String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Grid());
+    }
+  
+    
+
+    public static void main(String[] args)
+    {
+        Main main = new Main();
+        main.PathVisualization();
+    }
+} 
